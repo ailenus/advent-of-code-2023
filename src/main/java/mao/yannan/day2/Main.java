7670ec233ad45b791ff8df44f3b8ca73aab350eb
ailenus <ailenus@users.noreply.github.com>
2023-12-01 14:51:20 +0000

    day2 prep

--- src/main/java/mao/yannan/day2/Main.java
+++ src/main/java/mao/yannan/day2/Main.java
@@ -1,7 +1,25 @@
 package mao.yannan.day2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        String url = "src/main/resources/day2.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(url))) {
+            LOG.debug("Starting program");
+        } catch (IOException e) {
+            LOG.error(e.getMessage());
+        }
+
     }
+
 }
